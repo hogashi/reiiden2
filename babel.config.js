6ed82c2397e7b60ff924e02747d2bd970a1ef3c5
hogashi <azuma.ginkakuji@gmail.com>
2019-11-27 01:15:05 +0900

    lint babel conf

--- babel.config.js
+++ babel.config.js
@@ -1,13 +1,13 @@
 const presets = [
   [
-    "@babel/env",
+    '@babel/env',
     {
       targets: {
-        chrome: "76",
+        chrome: '76',
       },
     },
   ],
-  "@babel/preset-typescript",
+  '@babel/preset-typescript',
   // "@babel/preset-react",
 ];
 
